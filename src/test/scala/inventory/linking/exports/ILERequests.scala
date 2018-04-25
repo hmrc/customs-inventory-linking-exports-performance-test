d3c7dc8caa84efe424d35beab466e40c9f377733
Phil-hargreaves <33662315+Phil-hargreaves@users.noreply.github.com>
2018-04-25 13:05:35 +0100

    New Line added

--- src/test/scala/inventory/linking/exports/ILERequests.scala
+++ src/test/scala/inventory/linking/exports/ILERequests.scala
@@ -62,4 +62,3 @@ sealed abstract class User(val token: String, val version: String){
 
 sealed case class Csp(override val version: String) extends User("CSP", version)
 sealed case class NonCsp(override val version: String) extends User("Non CSP", version)
-
